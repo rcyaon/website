cb357aa5bec27b6c96c3b38131946e681b8d18fa
rcyaon <lena.conde-araujo@csedge.org>
2026-06-17 14:00:32 -0400

    Update main.js

--- main.js
+++ main.js
@@ -114,66 +114,64 @@ const PROJECTS = {
     noMedia: true,
     sections: [
       {
-        body: "echo \"A few of my favorites!\"",
-      },
-      {
         isHtml: true,
-        body: `<h3>Film</h3>
-<break><p>(in no order)</p>
-<break>
+        body: `
+        <h2>Film</h2>
+<p>(in no order)</p>
+<br>
 
 <p>Matt Johnson, Blackberry</p>
-<img src=”https://i.insider.com/6413370d5f4f54001919e790?width=700”>
-<break>
+<img src="https://i.insider.com/6413370d5f4f54001919e790?width=700" height="150">
+<br>
 
 <p>Joseph Losey, Mr. Klein</p>
-<img src=”https://criterion-production.s3.amazonaws.com/carousel-files/9PKdE5EHSiVAZekr3z4As2CdNlV7Enu5SWDZgoPm.jpeg” height=”150”> 
-<break>
+<img src="https://criterion-production.s3.amazonaws.com/carousel-files/9PKdE5EHSiVAZekr3z4As2CdNlV7Enu5SWDZgoPm.jpeg" height="150"> 
+<br>
 
 <p>Abbas Kiarostami, Taste of Cherry</p>
-<img src=”https://d1nslcd7m2225b.cloudfront.net/Pictures/1024x536/4/7/1/1231471_taste-of-cherry-still.jpg” height=”150”>
-<break>
+<img src="https://d1nslcd7m2225b.cloudfront.net/Pictures/1024x536/4/7/1/1231471_taste-of-cherry-still.jpg" height="150">
+<br>
 
 <p>Ken Loach, Kes</p>
-<img src=”https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS53CcnHsUQzDJWp05O7_KhDtGPdBU5v9xFNQ4iN3SsUd-RJ65AsHXu_4l&s=10” height=”150”>
-<break>
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS53CcnHsUQzDJWp05O7_KhDtGPdBU5v9xFNQ4iN3SsUd-RJ65AsHXu_4l&s=10" height="150">
+<br>
 
 <p>Víctor Erice, Close Your Eyes</p>
-<img src =”https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfJcrvyp52Nvo0MTFA9J4uwa7N2LwkIt8OegQsB7_WQfqF4N5GvB6WubU&s=10” height=”150”>
-<break>
-<break>
+<img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfJcrvyp52Nvo0MTFA9J4uwa7N2LwkIt8OegQsB7_WQfqF4N5GvB6WubU&s=10" height="150">
+<br>
+<br>
 
-<h3>Music</h3>
-<break><p>(in no order)</p>
-<break>
+<h2>Music</h2><p>(in no order)</p>
+<br>
 
 <p>caroline, caroline 2 (Rough Trade)</p>
 <img src="https://cdn.sonemic.net/i/600/w/443661b587a1cda3992eb689d25cc0c7/13248275/caroline-caroline-2-Cover-Art.jpg" width="200" height="200"> 
-<break>
+<br>
 
 <p>the now now and never, what is your name? (Reasonable)</p>
-<img src =”https://cdn.sonemic.net/i/600/w/a21a436204b531f720db6eccb32379a4/10901440/what-is-your-name-the-now-now-and-never-Cover-Art.jpg" width="200" height="200"> 
-<break>
+<img src ="https://cdn.sonemic.net/i/600/w/a21a436204b531f720db6eccb32379a4/10901440/what-is-your-name-the-now-now-and-never-Cover-Art.jpg" width="200" height="200"> 
+<br>
 
 <p>The Orchestra (For Now), Plan 76 (Not on label)</p>
 <img src="https://cdn.sonemic.net/i/600/w/365797ae36be85200652e048f56c1c5d/13719532/the-orchestra-for-now-plan-76-Cover-Art.jpg" width="200" height="200"> 
-<break>
+<br>
 
 <p>Soundtracks for the blind, Swans (Young God)</p>
 <img src="https://cdn.sonemic.net/i/600/w/a52a0ae9fd7bd2bee6439908d8a27610/14420516/swans-soundtracks-for-the-blind-Cover-Art.jpg" width="200" height="200"> 
-<break>
+<br>
 
 <p>Schlagenheim, Black Midi (Rough Trade)</p>
 <img src="https://cdn.sonemic.net/i/600/w/73aed220219cef1ff8dfb8afecef5288/8249847/black-midi-schlagenheim-Cover-Art.jpg" width="200" height="200">  
-<break>
+<br>
 
 <p>DJ Set, Mouseatouille (Remote Control)</p>
 <img src="https://cdn.sonemic.net/i/600/w/18387d776a3a2fdb1f2a1024a4aafb69/13559109/mouseatouille-dj-set-Cover-Art.jpg" width="200" height="200">  
-<break>
+<br>
 
 <p>Inferno, Boards of Canada (Warp)</p>
 <img src="https://cdn.sonemic.net/i/600/w/64b6056c5b5cec179f128b508a372529/14532480/boards-of-canada-inferno-Cover-Art.jpg" width="200" height="200">  
-        `,
+<br>
+`,
       },
     ],
     afterMedia: `<br><p class="favorites-return"><a href="#" onclick="openProjectModal('setlist'); return false;">&#x21B5; Return</a></p>`,
