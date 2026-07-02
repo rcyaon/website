5178c934c2bc909fc0211bb5fdd4823012a0279c
rcyaon <lena.conde-araujo@csedge.org>
2026-07-01 21:07:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -151,8 +151,8 @@ const PROJECTS = {
 <img src="https://cdn.sonemic.net/i/600/w/1406882e68da33a72be76b2b2fdd96e1/13277208/shes-green-wisteria-Cover-Art.jpg" width="200" height="200" style="border: 1px solid #6f6f6f;">  
 <br>
 
-<p>Black Country, New Road, For the First Time</p>
-<img src="https://cdn.sonemic.net/i/600/s/6bd10c3d453bd5516e1472dca97d7fee/8538705/black-country-new-road-for-the-first-time-Cover-Art.jpg" width="200" height="200" style="border: 1px solid #6f6f6f;"> 
+<p>Her New Knife, chrome is lullaby</p>
+<img src="https://cdn.sonemic.net/i/600/s/83cc8c6c2efa8df79bb40be2172a3d66/12587676/her-new-knife-chrome-is-lullaby-Cover-Art.jpg" width="200" height="200" style="border: 1px solid #6f6f6f;"> 
 
 <p>black midi, Hellfire</p>
 <img src="https://cdn.sonemic.net/i/600/w/8810e0cdb30d7bf8b69b23d9462b5c53/9932087/black-midi-hellfire-Cover-Art.jpg" width="200" height="200" style="border: 1px solid #6f6f6f;">  
